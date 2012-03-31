e7b92654827e69a005a88c268185814189a2b891
Karol Tatala <karol.tatala@gmail.com>
2012-03-31 19:20:50 +0200

    pierwszy

--- gen/akst/iwm01/R.java
+++ gen/akst/iwm01/R.java
@@ -8,13 +8,37 @@
 package akst.iwm01;
 
 public final class R {
+    public static final class array {
+        public static final int exercisesLabels=0x7f050000;
+        public static final int levelLabels=0x7f050001;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int LinearLayout01=0x7f060004;
+        public static final int TextView01=0x7f060005;
+        public static final int amount=0x7f060003;
+        public static final int amountLabel=0x7f060009;
+        public static final int button1=0x7f06000d;
+        public static final int button2=0x7f06000e;
+        public static final int button3=0x7f06000f;
+        public static final int exerciseName=0x7f060008;
+        public static final int level=0x7f060006;
+        public static final int linearLayout1=0x7f060001;
+        public static final int linearLayout2=0x7f06000b;
+        public static final int linearLayout3=0x7f06000c;
+        public static final int spinner1=0x7f060000;
+        public static final int startExerciseBtn=0x7f060007;
+        public static final int textView1=0x7f060002;
+        public static final int timeLabel=0x7f06000a;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int choose_exercise_layout=0x7f030000;
+        public static final int exercise_layout=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/akst/iwm01/AKST01Activity.java
+++ src/akst/iwm01/AKST01Activity.java
@@ -1,13 +1,48 @@
 package akst.iwm01;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class AKST01Activity extends Activity {
+	
+	Context ctx;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        ctx = getBaseContext();
+        
+        Button chooseExerciseBtn = (Button) findViewById(R.id.button3);
+        chooseExerciseBtn.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent intent = new Intent(ctx, ChooseExerciseActivity.class);	
+				startActivity(intent);
+			}
+		});
+        
+        Button startPlanBtn = (Button) findViewById(R.id.button1);
+        startPlanBtn.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO				
+			}
+		});
+        
+        Button continuePlanBtn = (Button) findViewById(R.id.button2);
+        continuePlanBtn.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO				
+			}
+		});
     }
 }
